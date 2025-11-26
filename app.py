66b856514032cee50dc2691ed21458357413f9f1
leeeg0301 <leeegyu6983@gmail.com>
2025-11-26 13:18:15 +0900

    app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def advanced_filter(keyword, bridges):
 # --------------------------------------
 # UI
 # --------------------------------------
-st.title("📸 교량 점검 사진 자동 파일명 생성기")
+st.title("교량 점검 사진 자동화")
 
 search_key = st.text_input("교량 검색 (예: ㅂ / 부 / 부산 / 산 / 천)")
 filtered = advanced_filter(search_key, bridges)
@@ -111,3 +111,4 @@ if uploaded and bridge and desc:
     if saved:
         st.session_state['uploaded_file'] = None   # 파일만 초기화
         st.experimental_rerun()
+
